bbbad3fdca41f3d5c1a83f3f678e19bf8344c832
Bob <crowcoder@gmail.com>
2018-10-25 20:27:42 -0400

    building search

--- site.js
+++ site.js
@@ -1,6 +1,15 @@
 let GlblModule = (function () {
 
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    })
+
     let all_data = {};
+
+    let dePriceLabel = document.querySelector("#selectedMaxPrice");
+    let dePriceRange = document.querySelector("#priceMax");;
 
     let load_data = function () {
         let theData = {};
@@ -10,36 +19,53 @@ let GlblModule = (function () {
             //console.log(JSON.stringify(myjson));
             buildProductList(myjson);
             all_data = myjson;
+        }).then(function () {
+
+            dePriceRange.addEventListener('change', function (element) {
+                let highestPrice = 0;
+                let the_products = all_data.products;
+
+                for (let i = 0; i < the_products.length; i++) {
+                    if (the_products[i].ListPrice > highestPrice) {
+                        highestPrice = the_products[i].ListPrice;
+                    }
+                }
+
+                let rangeValue = parseFloat(dePriceRange.value);
+                let max = (rangeValue / 100.00) * highestPrice;
+
+                dePriceLabel.innerHTML = formatter.format(max);
+            });
         });
     };
 
-    let img_error_handler = function(message, source, lineno, colno, error){
+    let img_error_handler = function (message, source, lineno, colno, error) {
         this.onerror = null;
         this.src = './images/NoImage.png';
     };
 
-    function get_category(cat_id){
+    function get_category(cat_id) {
         return cat_id === 1 ? 'Bikes' : cat_id === 2 ? 'Components' : cat_id === 3 ? 'Clothing' : cat_id === 4 ? 'Accessories' : 'Unknown';
     }
 
     function buildProductList(allData) {
         let product_list_div = document.querySelector('#product_list');
-        
-        for(let i = 0; i < allData.products.length; i++){
+
+        for (let i = 0; i < allData.products.length; i++) {
 
             let highlight = 'row_normal';
-            if(i % 2 === 0){
+            if (i % 2 === 0) {
                 highlight = 'row_highlighted';
             }
 
             //The entire row
             let product_row_div = document.createElement('div');
-            product_row_div.className = `row ${highlight}`;
-            
+            product_row_div.className = `row paddedrow ${highlight}`;
+
             //Cell to contain all details except image
             let product_details_div = document.createElement('div');
             product_details_div.className = "col-md-8";
-            
+
             // Product Name
             let product_description_div = document.createElement('div');
             let product_description = document.createTextNode(allData.products[i].ProductName);
@@ -78,12 +104,14 @@ let GlblModule = (function () {
             product_row_div.appendChild(add_to_cart_div);
 
             product_list_div.appendChild(product_row_div);
-            
+
         }
     }
 
+
+
     return {
         LoadData: load_data,
-        AllData : all_data
+        AllData: all_data
     }
 })();
